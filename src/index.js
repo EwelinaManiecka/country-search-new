0cbe919ead0b80a3c2855f87f164b5ceadccab0d
EwelinaManiecka <ewelina.maniecka@op.pl>
2022-10-26 20:58:14 +0200

    Update index.js
    infoCountry => ref

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const infoCountry = document.querySelector(".country-info");
 // infoCountry.addEventListener("click", showCountry);
 
 // function clean
-function cleanMarkup(infoCountry) {
-    return infoCountry.innerHTML = "";
+function cleanMarkup(ref) {
+    return ref.innerHTML = "";
   }
 
 function formatCountPeople(num) {
